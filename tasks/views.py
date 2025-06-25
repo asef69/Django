163cb223f5a417dde320dab7c43616b11a9861a7
Kazi Asef Kabir <asefkabir97@gmail.com>
2025-06-26 00:11:27 +0600

    7.5

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,10 @@
+from django.db.models import Q
 from django.shortcuts import render
 from django.http import HttpResponse
 from tasks.forms import TaskForm,TaskModelForm
 from tasks.models import Employee, Task
+from datetime import date
+from tasks.models import TaskDetails
 # Create your views here.
 def manager_dashboard(request):
     return render(request,"dashboard/manager-dashboard.html")
@@ -32,9 +35,13 @@ def create_task(request):
     return render(request, "task_form.html", {"form": form})
 def view_task(request):
     #rendering all the data form the tasks model
-    tasks=Task.objects.all()
-    task_3=Task.objects.get(id=3)
-    first_task=Task.objects.first()
-    return render(request, "show_task.html", {"tasks": tasks, "task_3": task_3, "first_task": first_task})
+    # tasks=Task.objects.all()
+    # task_3=Task.objects.get(id=3)
+    # first_task=Task.objects.first()
+    #tasks=Task.objects.filter(due_date=date.today())
+    #tasks=TaskDetails.objects.exclude(priority='L').select_related('task')
+    #tasks=Task.objects.filter(title__icontains="paper")
+    tasks=Task.objects.filter(Q(status='PENDING') | Q(status='IN_PROGRESS'))
+    return render(request, "show_task.html", {"tasks": tasks})
 
 
